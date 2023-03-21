22afeaf8d9655de8521bcf8ebebfe8096fd97fad
mcold <mholodnuk@gmail.com>
2023-03-21 23:19:04 +0300

    + standard generation

--- prj.py
+++ prj.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 import typer
-from typing import Optional
 import os
 
 app = typer.Typer()
@@ -9,8 +8,14 @@ dir = ''
 sep = '\\'
 file_path_start = '1,4,,,'
 prj_start = 'PL/SQL Developer Project'
+prj_ext = 'prj'
 l_accept_ext = ['sql']
+token_with =''
 
+clear = lambda: os.system('cls')
+dir_name = lambda: os.path.abspath(dir).rpartition(os.sep)[-1].upper()
+prj_abs_file = lambda: os.path.abspath(dir) + os.sep + dir_name() + '.' + prj_ext
+prj_abs_file_token = lambda: os.path.abspath(dir) + os.sep + dir_name() + '_' + token_with.upper() + '.' + prj_ext
 
 def is_exists_by_token(l_str: list, token: str = None, token_without: str = None) -> bool:
     if not token and not token_without: return True
@@ -24,15 +29,23 @@ def is_exists_by_token(l_str: list, token: str = None, token_without: str = None
         return True
     return False
 
-def get_prj_dir() -> str:
-    while True:
-        dir = input("\nEnter directory for project: \n")
-        if not os.path.exists(dir): continue
-        return dir
+def get_prj_dir(prj_dir: str = None) -> str:
+    if not os.path.exists(prj_dir):
+        print('\nNot correct directory!\n\n')
+        while True:
+            clear()
+            prj_dir = input("\nEnter directory for project: \n")
+            if not os.path.exists(prj_dir): 
+                continue
+            else:
+                print('\nNot correct directory!\n\n')
+            return prj_dir
+    else:
+        return prj_dir        
 
-def set_prj_dir():
+def set_prj_dir(prj_dir: str = None):
     global dir
-    dir = get_prj_dir()
+    dir = get_prj_dir(prj_dir)
 
 def get_groups(token: str = None, token_without: str = None) -> list:
     s_groups = set()
@@ -53,7 +66,6 @@ def is_acceptable_ext(file: str) -> bool:
     for ext in l_accept_ext:
         if file.lower().endswith('.' + ext.lower()) > 0: return True
     return False
-
 
 def get_files(token: str = None, token_without: str = None) -> list:
     l_files = list()
@@ -88,7 +100,6 @@ def get_files_order(l_files: list, group_name: str) -> str:
         if f_path.lower().startswith(group_name.lower() + '\\'): l_num.append(i)
     return ','.join([str(x) for x in l_num])
 
-
 def get_order_groups(l_groups: list, token: str = None, token_without: str = None) -> list:
     l_files = get_files(token=token, token_without=token_without)
     l_gr_str = list()
@@ -104,77 +115,50 @@ def get_order_groups_str(token: str = None, token_without: str = None) -> str:
 def get_order_groups_header_str(token: str = None, token_without: str = None):
     return '\n'.join(['[Groups]', get_order_groups_str(token=token, token_without=token_without)])
 
-
 def get_prj(token: str = None, token_without: str = None) -> str:
     return '\n\n'.join([prj_start, get_order_groups_header_str(token=token, token_without=token_without), get_order_files_header_str(token=token, token_without=token_without)])
 
 
 ############## Application ####################################
 @app.command(help='Full project output')
-def get_prj_output():
-    set_prj_dir()
+def get_prj_output(prj_dir: str):
+    set_prj_dir(prj_dir = prj_dir)
     print(get_prj())
 
 @app.command(help='Project output with files containing token')
-def get_prj_output_by_token(token: str = typer.Argument(str, help="Only files with token")):
-    set_prj_dir()
+def get_prj_output_by_token(prj_dir: str, token: str = typer.Argument(str, help="Only files with token")):
+    set_prj_dir(prj_dir = prj_dir)
     print(get_prj(token = token))
 
 @app.command(help='Project output without files containing token')
-def get_prj_output_without_token(token: str = typer.Argument(str, help="Only files without token")):
-    set_prj_dir()
+def get_prj_output_without_token(prj_dir: str, token: str = typer.Argument(str, help="Only files without token")):
+    set_prj_dir(prj_dir = prj_dir)
     print(get_prj(token_without = token))
 
 @app.command(help='Generate project')
-def gen_prj_output():
-    set_prj_dir()
-    # print(get_prj())
-    pass
+def gen_prj_output(prj_dir: str):
+    set_prj_dir(prj_dir = prj_dir)
+    with open(prj_abs_file(), 'w') as f: f.write(get_prj(token_without=token))
 
 @app.command(help='Generate project with files containing token')
-def gen_prj_by_token(token: str = typer.Argument(str, help="Only files with token")):
-    set_prj_dir()
-    # print(get_prj(token = token))
-    pass
+def gen_prj_by_token(prj_dir: str, token: str = typer.Argument(str, help="Only files with token")):
+    global token_with
+    token_with = token
+    set_prj_dir(prj_dir = prj_dir)
+    with open(prj_abs_file_token(), 'w') as f: f.write(get_prj(token=token))
 
 @app.command(help='Generate project without files containing token')
-def gen_prj_without_token(token: str = typer.Argument(str, help="Only files without token")):
-    set_prj_dir()
-    # print(get_prj(token_without = token))
-    pass
+def gen_prj_without_token(prj_dir: str, token: str = typer.Argument(str, help="Only files without token")):
+    set_prj_dir(prj_dir = prj_dir)
+    with open(prj_abs_file(), 'w') as f: f.write(get_prj(token_without=token))
 
-## TODO: standard project
+@app.command(help='Generate typical project')
+def gen_prj_standard(prj_dir: str):
+    gen_prj_without_token(prj_dir=prj_dir, token='rollback')
 
-## TODO: standard rollback project
-
-# @app.command()
-# def main(name: str = typer.Argument(str, help="The name of the user to greet")):
-#     print(f"Hello {name}")
-
+@app.command(help='Generate typical project rollback')
+def gen_prj_standard_rollback(prj_dir: str):
+    gen_prj_by_token(prj_dir=prj_dir, token='rollback')
 
 if __name__ == "__main__":
     app()
-    # populate_dir()
-    # print(get_groups())
-    # print(get_files())
-
-    # print(get_files())
-    # with open('res.txt', 'w') as f:
-    #     for ff in get_files():
-    #         f.write(ff + '\n')
-
-    # main project
-    # print(get_files(token_without = 'rollback'))
-    # print(get_groups())
-    # print(get_order_files_str())
-
-    # rollback project
-    # print(get_files(token = 'rollback'))
-    # print(get_order_groups(get_groups()))
-
-    # header groups
-    # print(get_order_groups_header_str())
-
-    # print(get_prj(token_without='rollback'))
-    # print(get_prj(token='rollback'))
-    # print(get_prj(token_without='rollback'))
